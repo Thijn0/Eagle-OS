5b73941d7a492d1e814eb3ddc7b8cc7284c7bea3
Thijn <103674164+Thijn0@users.noreply.github.com>
2024-06-19 15:14:49 +0000

    Login System [Beta Version]

---
+++ Database/login.py
@@ -0,0 +1,72 @@
+def register():
+    db = open("Database/data.txt", "r")
+    username = input("Create Username: ")
+    password = input("Create Password: ")
+    password1 = input("Confirm Password: ")
+    d = []
+    f = []
+    for i in db:
+        a,b = i.split(", ")
+        b = b.strip()
+        d.append(a)
+        f.append(b)
+    data = dict(zip(d, f))
+  
+    if password != password1:
+        print("Passwords don't match, restart.")
+        register()
+    else:
+      if len(password)<=6:
+        print("Password too short, restart.")
+        register()
+     # elif username in db:
+     #   print("Username exists, restart.")
+     #   register()
+      else:
+          db = open("Database/data.txt", "a")
+          db.write(username+", "+password+"\n")
+          print("User created.")
+
+
+def access():
+    db = open("Database/data.txt", "r")
+    username = input("Enter your Username: ")
+    password = input("Enter your Password: ")
+
+    if not len(username or password)<1:
+      d = []
+      f = []
+      for i in db:
+          a,b = i.split(", ")
+          b = b.strip()
+          d.append(a)
+          f.append(b)
+      data = dict(zip(d, f))
+
+      try:
+         if data [username]:
+            try:
+                if password == data[username]:
+                    print("Login Succesfull.")
+                    print("Welcome back," , username)
+                else:
+                    print("Username or Password incorrect.")
+
+            except:
+                print("Incorrect password or username.")          
+         else:
+              print("Username doesn't exist.") 
+      except:
+          print("Login Error.")
+    else:
+      print("Please enter a value.")
+
+def home(option=None):
+    option = input("Login | Register: ")
+    if option == "Login":
+        access()
+    elif option == "Register":
+        register()
+    else:
+        print("Please enter an option.")
+home()
--- main.py
+++ main.py
@@ -6,3 +6,22 @@ print("| |___  |  _  | | |     | |      | |___  ")
 print("|  ___| | | | | | |     | |      |  ___| ")
 print("| |___  | | | | | |__|| | |____  | |___  ")
 print("|_____| |_| |_| |_____| |______| |_____| ")
+
+print("\nLoading")
+print(".")
+time.sleep(1)
+print("..")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("....")
+print(".....")
+time.sleep(2)
+print("System succesfully started.")
+
+print("\nWelcome to Eagle OS 2.2!\n\n")
+
+loginchoice = input("test")
+
+
+
