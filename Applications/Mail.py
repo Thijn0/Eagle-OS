b2e24875ca69401f906dd622029c2d0c07183993
Thijn <103674164+Thijn0@users.noreply.github.com>
2024-06-20 15:57:51 +0000

    [Applications] V1.0.0

---
+++ Applications/Mail.py
@@ -0,0 +1,13 @@
+import time
+
+print("Welcome to Eagle Mail")
+time.sleep(1)
+print("You have 3 mails")
+print("which one do you want to open?")
+choice = input("1, 2, 3")
+if choice == "1":
+  print("Mail by: EagleOSsystem")
+  print("Welcome to Eagle OS,we are delighted to have you here")
+  print("We hope you enjoy your stay with us")
+elif choice == "2":
+  print
---
+++ Applications/Settings.py
@@ -0,0 +1,11 @@
+print("[===] Settings [===]")
+
+print("1. Update")
+
+
+setting = input("\n> ")
+
+if input == "1":
+  print("You're up to date.")
+  print("\n\n Version: 3.0.0")
+  print("--------------")
